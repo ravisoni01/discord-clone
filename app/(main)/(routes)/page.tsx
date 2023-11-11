74359b4d5fb0bb7fa00b9a537e897c691e85f1ee
Ravi Soni <r7679242@gmail.com>
2023-11-12 00:03:50 +0530

    project setup

---
+++ app/(main)/(routes)/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="text-3xl font-bold text-cyan-300">
+      <p>Hello!</p>
+      <Button>Submit</Button>
+    </div>
+  );
+}
